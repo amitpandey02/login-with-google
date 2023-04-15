348f7a8e05a829edb68e907ec92819bd375d0564
amitpandey02 <amitpandey@nimapinfotech.com>
2023-04-15 11:52:51 +0530

    initial

---
+++ LoginWithGoogle_UsingIdentity/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LoginWithGoogle_UsingIdentity
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ LoginWithGoogle_UsingIdentity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoginWithGoogle_UsingIdentity.MvcApplication" Language="C#" %>
